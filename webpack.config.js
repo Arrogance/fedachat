543325fbd95cb7c48791b5111781d2498931fafc
Manuel Raya <manuel@arrogance.es>
2020-03-28 20:50:55 +0100

    Try to fix the build process.

--- webpack.config.js
+++ webpack.config.js
@@ -14,84 +14,87 @@ const PATHS = {
     sounds: path.join(__dirname, 'assets', 'sounds')
 };
 
-module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    entry: {
-        app: PATHS.src + '/app.js'
-    },
-    output: {
-        path: PATHS.build,
-        filename: 'app.js'
-    },
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.common.js'
-        }
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                test: '/.js(?.*)?$/i'
+module.exports = env => {
+    return {
+        mode: env.NODE_ENV === 'production' ? 'production' : 'development',
+        entry: {
+            app: PATHS.src + '/app.js'
+        },
+        output: {
+            path: PATHS.build,
+            filename: 'app.js'
+        },
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.common.js'
+            }
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    test: '/.js(?.*)?$/i'
+                })
+            ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        env.NODE_ENV === 'production'
+                            ? 'style-loader'
+                            : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: 'css-loader'
+                },
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    use: [
+                        'file-loader',
+                        {
+                            loader: 'image-webpack-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: 'url-loader?limit=10000&mimetype=application/font-woff'
+                },
+                {
+                    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                    use: 'url-loader?limit=10000&mimetype=application/octet-stream'
+                },
+                {
+                    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                    use: 'file-loader'
+                },
+                {
+                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                    use: 'url-loader?limit=10000&mimetype=image/svg+xml'
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                }
+            ]
+        },
+        plugins: [
+            new CopyWebpackPlugin([
+                { from: PATHS.images, to: path.join(PATHS.build, 'images') },
+                { from: PATHS.sounds, to: path.join(PATHS.build, 'sounds') }
+            ]),
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin(),
+            new Dotenv({
+                path: path.resolve(__dirname, '.env'),
+                systemvars: true
             })
         ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    process.env.NODE_ENV === 'production'
-                        ? 'style-loader'
-                        : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: 'css-loader'
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                use: 'url-loader?limit=10000&mimetype=application/font-woff'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                use: 'url-loader?limit=10000&mimetype=application/octet-stream'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                use: 'file-loader'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                use: 'url-loader?limit=10000&mimetype=image/svg+xml'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            { from: PATHS.images, to: path.join(PATHS.build, 'images') },
-            { from: PATHS.sounds, to: path.join(PATHS.build, 'sounds') }
-        ]),
-        new VueLoaderPlugin(),
-        new MiniCssExtractPlugin(),
-        new Dotenv({
-            path: path.resolve(__dirname, '.env')
-        })
-    ]
+    }
 };
