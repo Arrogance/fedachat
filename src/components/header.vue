27b09e65ea126543c373e662c94da678a85ad99b
Manuel Raya <manuel@arrogance.es>
2020-04-03 19:41:24 +0200

    Improve responsive rendering for mobile devices.

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,6 @@
 <template>
     <section id="navbar">
-        <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
             <b-container fluid="">
                 <b-navbar-brand href="#" class="hide-375">
                     <h1>FedaChat</h1>
@@ -24,7 +24,7 @@
                         <b-button @click="toggleModal('options-modal')"> <b-icon-gear></b-icon-gear> <span class="sr-only">Opciones</span></b-button>
                     </b-nav-item>
                     <b-nav-item class="hidden show-425" v-on:click="toggleChat">
-                        <b-button>
+                        <b-button :variant="chatShown ? 'info' : 'secondary'">
                             <b-icon-chat-square-dots v-if="!chatShown"></b-icon-chat-square-dots>
                             <b-icon-chat-square-dots-fill v-else></b-icon-chat-square-dots-fill>
                         </b-button>
