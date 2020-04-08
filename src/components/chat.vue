274295652c2af3133c62d1d4eeba4f5050dbecfa
Manuel Raya <manuel@arrogance.es>
2020-04-09 00:12:05 +0200

    Improve the regexp.

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -133,7 +133,8 @@
                     let realUserName = userName.substring(1);
                     _this.$root.users.forEach(function(user) {
                         if(user.userName.toLowerCase() === realUserName.trim().toLowerCase()) {
-                            let regexp = new RegExp("/((?!([<strong>])).|^)(@[" + realUserName + "]+)/gm", "gm");
+                            // let regexp = new RegExp("/((?<![<strong>])@[" + realUserName + "]+)/gm", "gm");
+                            let regexp = new RegExp("((?!([<strong>])).|^)(?:@"+ realUserName + ")", "gm");
                             message.content = message.content.replace(regexp, '<strong>' + userName + '</strong>');
 
                             if(_this.$root.user.userName.toLowerCase() === realUserName.trim().toLowerCase() && -1 === alreadyNotified.indexOf(realUserName)) {
