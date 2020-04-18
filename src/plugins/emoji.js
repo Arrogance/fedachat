8f79e24fe24cbe91cf4883f2c937f6a88048ac19
Manuel Raya <manuel@arrogance.es>
2020-04-19 00:31:54 +0200

    Avoid applying emojis to single character words.

--- src/plugins/emoji.js
+++ src/plugins/emoji.js
@@ -22,6 +22,10 @@ const customMapping = {
 export default {
     processText: function(text) {
         text.split(' ').forEach(function(value) {
+            if (value.length <= 1) {
+                return;
+            }
+
             let firstChar = value[0];
             let lastChar = value[value.length - 1];
 
@@ -32,6 +36,7 @@ export default {
                 return;
             }
 
+            console.log(value);
             let emoji = toEmoji.get(value) || toEmoji.get(customMapping[value]);
             if (emoji === undefined) {
                 return;
