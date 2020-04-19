78af24e633593b6551559490aa88eec525379351
Manuel Raya <manuel@arrogance.es>
2020-04-19 02:18:19 +0200

    Emoji improvements.

--- src/plugins/emoji.js
+++ src/plugins/emoji.js
@@ -2,39 +2,51 @@ import toEmoji from 'emoji-name-map';
 import unicodeEmoji from 'emoji-unicode';
 
 const customMapping = {
-    '<3': 'heart',
-    '</3': 'broken_heart',
-    ':)': 'smile',
-    ':(': 'slightly_frowning_face',
-    ':P': 'yum',
-    ':p': 'yum',
-    ':D': 'grinning',
-    ':d': 'grinning',
-    ':O': 'open_mouth',
-    ':o': 'open_mouth',
-    ':|': 'neutral_face',
-    ':/': 'unsure',
-    ':*': 'kissing_heart',
-    '(:': 'upside_down_face',
-    xD: 'rofl',
-    xd: 'rofl'
+    '<3': ':heart:',
+    '</3': ':broken_heart:',
+    ':)': ':smile:',
+    ':(': ':slightly_frowning_face:',
+    ':P': ':yum:',
+    ':p': ':yum:',
+    ':D': ':grinning:',
+    ':d': ':grinning:',
+    ':O': ':open_mouth:',
+    ':o': ':open_mouth:',
+    ':|': ':neutral_face:',
+    ':/': ':unsure:',
+    ':*': ':kissing_heart:',
+    '(:': ':upside_down_face:',
+    xD: ':rofl:',
+    xd: ':rofl:'
 };
 
 export default {
     processText: function(text) {
+        if (text === undefined) {
+            return text;
+        }
+
         text.split(' ').forEach(function(value) {
             if (value.length <= 1) {
-                return;
+                return text;
+            }
+
+            let originalValue = value;
+            if (customMapping[value]) {
+                value = customMapping[value];
             }
 
             try {
-                let emoji = toEmoji.get(value) || toEmoji.get(customMapping[value]);
+                let matches = value.match(/:(\w+):/gm);
+                let match = matches[0];
+
+                let emoji = toEmoji.get(match);
                 if (emoji === undefined) {
-                    return;
+                    return text;
                 }
 
                 let emojiCode = unicodeEmoji(emoji).split(' ');
-                text = text.replace(value, '&#x' + emojiCode[0]);
+                text = text.replace(originalValue, '&#x' + emojiCode[0]);
             } catch {}
         });
 
